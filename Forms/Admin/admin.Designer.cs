a1516e3b975bc0ece8e874c60bdb8e60125f14f8
Hirozanami07 <Stephensteveg07@gmail.com>
2025-06-18 02:53:42 +0800

    Admin dashboard

--- Forms/Admin/admin.Designer.cs
+++ Forms/Admin/admin.Designer.cs
@@ -35,6 +35,7 @@
             this.foreverButton3 = new ReaLTaiizor.Controls.ForeverButton();
             this.foreverButton4 = new ReaLTaiizor.Controls.ForeverButton();
             this.cuiPanel1 = new CuoreUI.Controls.cuiPanel();
+            this.cuiPanel2 = new CuoreUI.Controls.cuiPanel();
             this.SuspendLayout();
             // 
             // cuiGradientPanel1
@@ -42,7 +43,6 @@
             this.cuiGradientPanel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.cuiGradientPanel1.GradientAngle = 0F;
             this.cuiGradientPanel1.Location = new System.Drawing.Point(0, 0);
-            this.cuiGradientPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cuiGradientPanel1.Name = "cuiGradientPanel1";
             this.cuiGradientPanel1.OutlineThickness = 1F;
             this.cuiGradientPanel1.PanelColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
@@ -50,7 +50,7 @@
             this.cuiGradientPanel1.PanelOutlineColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
             this.cuiGradientPanel1.PanelOutlineColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
             this.cuiGradientPanel1.Rounding = new System.Windows.Forms.Padding(8);
-            this.cuiGradientPanel1.Size = new System.Drawing.Size(397, 768);
+            this.cuiGradientPanel1.Size = new System.Drawing.Size(189, 768);
             this.cuiGradientPanel1.TabIndex = 0;
             // 
             // cuiPictureBox1
@@ -60,14 +60,14 @@
             this.cuiPictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.cuiPictureBox1.Content = null;
             this.cuiPictureBox1.ImageTint = System.Drawing.Color.White;
-            this.cuiPictureBox1.Location = new System.Drawing.Point(37, 27);
-            this.cuiPictureBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.cuiPictureBox1.Location = new System.Drawing.Point(13, 22);
+            this.cuiPictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.cuiPictureBox1.Name = "cuiPictureBox1";
             this.cuiPictureBox1.OutlineThickness = 1F;
             this.cuiPictureBox1.PanelOutlineColor = System.Drawing.Color.Empty;
             this.cuiPictureBox1.Rotation = 0;
             this.cuiPictureBox1.Rounding = new System.Windows.Forms.Padding(8);
-            this.cuiPictureBox1.Size = new System.Drawing.Size(325, 179);
+            this.cuiPictureBox1.Size = new System.Drawing.Size(158, 146);
             this.cuiPictureBox1.TabIndex = 1;
             // 
             // foreverButton1
@@ -76,11 +76,10 @@
             this.foreverButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
             this.foreverButton1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.foreverButton1.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.foreverButton1.Location = new System.Drawing.Point(0, 243);
-            this.foreverButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.foreverButton1.Location = new System.Drawing.Point(0, 197);
             this.foreverButton1.Name = "foreverButton1";
             this.foreverButton1.Rounded = false;
-            this.foreverButton1.Size = new System.Drawing.Size(397, 70);
+            this.foreverButton1.Size = new System.Drawing.Size(189, 57);
             this.foreverButton1.TabIndex = 2;
             this.foreverButton1.Text = "Dashboard";
             this.foreverButton1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
@@ -91,11 +90,10 @@
             this.foreverButton2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
             this.foreverButton2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.foreverButton2.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.foreverButton2.Location = new System.Drawing.Point(0, 321);
-            this.foreverButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.foreverButton2.Location = new System.Drawing.Point(0, 261);
             this.foreverButton2.Name = "foreverButton2";
             this.foreverButton2.Rounded = false;
-            this.foreverButton2.Size = new System.Drawing.Size(397, 70);
+            this.foreverButton2.Size = new System.Drawing.Size(189, 57);
             this.foreverButton2.TabIndex = 3;
             this.foreverButton2.Text = "Manage Accounts";
             this.foreverButton2.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
@@ -106,11 +104,10 @@
             this.foreverButton3.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
             this.foreverButton3.Cursor = System.Windows.Forms.Cursors.Hand;
             this.foreverButton3.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.foreverButton3.Location = new System.Drawing.Point(0, 398);
-            this.foreverButton3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.foreverButton3.Location = new System.Drawing.Point(0, 323);
             this.foreverButton3.Name = "foreverButton3";
             this.foreverButton3.Rounded = false;
-            this.foreverButton3.Size = new System.Drawing.Size(397, 70);
+            this.foreverButton3.Size = new System.Drawing.Size(189, 57);
             this.foreverButton3.TabIndex = 4;
             this.foreverButton3.Text = "Manage Services";
             this.foreverButton3.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
@@ -121,11 +118,10 @@
             this.foreverButton4.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
             this.foreverButton4.Cursor = System.Windows.Forms.Cursors.Hand;
             this.foreverButton4.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.foreverButton4.Location = new System.Drawing.Point(0, 476);
-            this.foreverButton4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.foreverButton4.Location = new System.Drawing.Point(0, 387);
             this.foreverButton4.Name = "foreverButton4";
             this.foreverButton4.Rounded = false;
-            this.foreverButton4.Size = new System.Drawing.Size(397, 70);
+            this.foreverButton4.Size = new System.Drawing.Size(189, 57);
             this.foreverButton4.TabIndex = 5;
             this.foreverButton4.Text = "System Logs";
             this.foreverButton4.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
@@ -133,21 +129,36 @@
             // cuiPanel1
             // 
             this.cuiPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cuiPanel1.Location = new System.Drawing.Point(397, 0);
+            this.cuiPanel1.Location = new System.Drawing.Point(189, 0);
+            this.cuiPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.cuiPanel1.Name = "cuiPanel1";
             this.cuiPanel1.OutlineThickness = 1F;
             this.cuiPanel1.PanelColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
             this.cuiPanel1.PanelOutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
             this.cuiPanel1.Rounding = new System.Windows.Forms.Padding(8);
-            this.cuiPanel1.Size = new System.Drawing.Size(969, 768);
+            this.cuiPanel1.Size = new System.Drawing.Size(1177, 768);
             this.cuiPanel1.TabIndex = 6;
+            // 
+            // cuiPanel2
+            // 
+            this.cuiPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cuiPanel2.Location = new System.Drawing.Point(189, 0);
+            this.cuiPanel2.Margin = new System.Windows.Forms.Padding(2);
+            this.cuiPanel2.Name = "cuiPanel2";
+            this.cuiPanel2.OutlineThickness = 1F;
+            this.cuiPanel2.PanelColor = System.Drawing.Color.Cyan;
+            this.cuiPanel2.PanelOutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiPanel2.Rounding = new System.Windows.Forms.Padding(8);
+            this.cuiPanel2.Size = new System.Drawing.Size(1177, 66);
+            this.cuiPanel2.TabIndex = 7;
             // 
             // admin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.cuiPanel2);
             this.Controls.Add(this.cuiPanel1);
             this.Controls.Add(this.foreverButton4);
             this.Controls.Add(this.foreverButton3);
@@ -156,7 +167,6 @@
             this.Controls.Add(this.cuiPictureBox1);
             this.Controls.Add(this.cuiGradientPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "admin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "admin";
@@ -175,5 +185,6 @@
         private ReaLTaiizor.Controls.ForeverButton foreverButton3;
         private ReaLTaiizor.Controls.ForeverButton foreverButton4;
         private CuoreUI.Controls.cuiPanel cuiPanel1;
+        private CuoreUI.Controls.cuiPanel cuiPanel2;
     }
 }
