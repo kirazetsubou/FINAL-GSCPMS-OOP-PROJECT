231979a313149e549fd11885d7e4a7e4740acd8a
Mark Merto <markmerto2004@gmail.com>
2025-06-18 04:18:27 +0800

    saxasx

--- Forms/Admin/admindashboard.Designer.cs
+++ Forms/Admin/admindashboard.Designer.cs
@@ -28,104 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
             this.rPanel3 = new GSPMS.RPanel();
             this.rPanel2 = new GSPMS.RPanel();
             this.rPanel1 = new GSPMS.RPanel();
             this.bigLabel1 = new ReaLTaiizor.Controls.BigLabel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // panel4
             // 
-            this.panel1.Controls.Add(this.panel5);
-            this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.panel4);
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.rPanel3);
-            this.panel1.Controls.Add(this.rPanel2);
-            this.panel1.Controls.Add(this.rPanel1);
-            this.panel1.Controls.Add(this.bigLabel1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1333, 715);
-            this.panel1.TabIndex = 0;
-            // 
-            // rPanel3
-            // 
-            this.rPanel3.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.rPanel3.BorderColor = System.Drawing.Color.White;
-            this.rPanel3.Edge = 20;
-            this.rPanel3.Location = new System.Drawing.Point(541, 117);
-            this.rPanel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.rPanel3.Name = "rPanel3";
-            this.rPanel3.Size = new System.Drawing.Size(183, 101);
-            this.rPanel3.TabIndex = 4;
-            // 
-            // rPanel2
-            // 
-            this.rPanel2.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.rPanel2.BorderColor = System.Drawing.Color.White;
-            this.rPanel2.Edge = 20;
-            this.rPanel2.Location = new System.Drawing.Point(305, 117);
-            this.rPanel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.rPanel2.Name = "rPanel2";
-            this.rPanel2.Size = new System.Drawing.Size(183, 101);
-            this.rPanel2.TabIndex = 3;
-            // 
-            // rPanel1
-            // 
-            this.rPanel1.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.rPanel1.BorderColor = System.Drawing.Color.White;
-            this.rPanel1.Edge = 20;
-            this.rPanel1.Location = new System.Drawing.Point(85, 117);
-            this.rPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.rPanel1.Name = "rPanel1";
-            this.rPanel1.Size = new System.Drawing.Size(183, 101);
-            this.rPanel1.TabIndex = 2;
-            // 
-            // bigLabel1
-            // 
-            this.bigLabel1.AutoSize = true;
-            this.bigLabel1.BackColor = System.Drawing.Color.Transparent;
-            this.bigLabel1.Font = new System.Drawing.Font("Segoe UI", 25F);
-            this.bigLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
-            this.bigLabel1.Location = new System.Drawing.Point(460, 13);
-            this.bigLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.bigLabel1.Name = "bigLabel1";
-            this.bigLabel1.Size = new System.Drawing.Size(362, 57);
-            this.bigLabel1.TabIndex = 1;
-            this.bigLabel1.Text = "Admin Dashboard";
-            // 
-            // panel5
-            // 
-            this.panel5.Location = new System.Drawing.Point(1008, 218);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(227, 484);
-            this.panel5.TabIndex = 23;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(960, 103);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(85, 16);
-            this.label8.TabIndex = 29;
-            this.label8.Text = "System Logs";
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Location = new System.Drawing.Point(26, 608);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(584, 131);
+            this.panel4.TabIndex = 32;
             // 
             // label7
             // 
@@ -136,6 +60,14 @@
             this.label7.TabIndex = 28;
             this.label7.Text = "Patients Overview";
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(35, 448);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(854, 154);
+            this.panel3.TabIndex = 31;
+            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -144,6 +76,14 @@
             this.label6.Size = new System.Drawing.Size(141, 16);
             this.label6.TabIndex = 27;
             this.label6.Text = "Receptionist Overview";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(86, 276);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(857, 145);
+            this.panel2.TabIndex = 30;
             // 
             // label5
             // 
@@ -154,41 +94,67 @@
             this.label5.TabIndex = 26;
             this.label5.Text = "Doctors Overview";
             // 
-            // panel4
+            // rPanel3
             // 
-            this.panel4.Controls.Add(this.label7);
-            this.panel4.Location = new System.Drawing.Point(97, 571);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(781, 131);
-            this.panel4.TabIndex = 25;
+            this.rPanel3.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.rPanel3.BorderColor = System.Drawing.Color.White;
+            this.rPanel3.Edge = 20;
+            this.rPanel3.Location = new System.Drawing.Point(530, 133);
+            this.rPanel3.Margin = new System.Windows.Forms.Padding(4);
+            this.rPanel3.Name = "rPanel3";
+            this.rPanel3.Size = new System.Drawing.Size(183, 101);
+            this.rPanel3.TabIndex = 29;
             // 
-            // panel3
+            // rPanel2
             // 
-            this.panel3.Controls.Add(this.label6);
-            this.panel3.Location = new System.Drawing.Point(100, 434);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(785, 131);
-            this.panel3.TabIndex = 24;
+            this.rPanel2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.rPanel2.BorderColor = System.Drawing.Color.White;
+            this.rPanel2.Edge = 20;
+            this.rPanel2.Location = new System.Drawing.Point(294, 133);
+            this.rPanel2.Margin = new System.Windows.Forms.Padding(4);
+            this.rPanel2.Name = "rPanel2";
+            this.rPanel2.Size = new System.Drawing.Size(183, 101);
+            this.rPanel2.TabIndex = 28;
             // 
-            // panel2
+            // rPanel1
             // 
-            this.panel2.Controls.Add(this.label5);
-            this.panel2.Location = new System.Drawing.Point(97, 260);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 131);
-            this.panel2.TabIndex = 22;
+            this.rPanel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.rPanel1.BorderColor = System.Drawing.Color.White;
+            this.rPanel1.Edge = 20;
+            this.rPanel1.Location = new System.Drawing.Point(74, 133);
+            this.rPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.rPanel1.Name = "rPanel1";
+            this.rPanel1.Size = new System.Drawing.Size(183, 101);
+            this.rPanel1.TabIndex = 27;
+            // 
+            // bigLabel1
+            // 
+            this.bigLabel1.AutoSize = true;
+            this.bigLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.bigLabel1.Font = new System.Drawing.Font("Segoe UI", 25F);
+            this.bigLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.bigLabel1.Location = new System.Drawing.Point(449, 29);
+            this.bigLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.bigLabel1.Name = "bigLabel1";
+            this.bigLabel1.Size = new System.Drawing.Size(362, 57);
+            this.bigLabel1.TabIndex = 26;
+            this.bigLabel1.Text = "Admin Dashboard";
             // 
             // admindashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.rPanel3);
+            this.Controls.Add(this.rPanel2);
+            this.Controls.Add(this.rPanel1);
+            this.Controls.Add(this.bigLabel1);
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "admindashboard";
-            this.Size = new System.Drawing.Size(1333, 715);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.Size = new System.Drawing.Size(969, 768);
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.panel3.ResumeLayout(false);
@@ -196,23 +162,21 @@
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private ReaLTaiizor.Controls.BigLabel bigLabel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
         private GSPMS.RPanel rPanel3;
         private GSPMS.RPanel rPanel2;
         private GSPMS.RPanel rPanel1;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel2;
+        private ReaLTaiizor.Controls.BigLabel bigLabel1;
     }
 }
